962672508d4c49fd38a6e33da7c012b5f488765f
Igor de Castro <igordecastro07@gmail.com>
2023-06-04 21:14:25 -0300

    initial commit

---
+++ projeto22-autoral-front-end/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between ${inter.className}`}
+    >
+      <div className="navbar">Estou na main</div>
+    </main>
+  )
+}
