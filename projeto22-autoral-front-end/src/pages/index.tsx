1ed6e92e19e7838bb494275555717622f26bdcd3
Igor de Castro <igordecastro07@gmail.com>
2023-06-23 10:03:41 -0300

    feat: div container on index.tsx

--- projeto22-autoral-front-end/src/pages/index.tsx
+++ projeto22-autoral-front-end/src/pages/index.tsx
@@ -29,27 +29,34 @@ export default function Home() {
       <div className="flex items-center justify-around fixed top-0 left-0 bg-slate-500 w-full text-white h-12">
         <img src="/logo.png" alt="Clínica" className="h-full w-auto" />
         <h1>Clínica Estética</h1>
-        <div className="border-2 rounded-lg flex items-center w-9" onClick={() => router.replace('/sign-in')}>
+        <div
+          className="border-2 rounded-lg flex items-center w-9"
+          onClick={() => router.replace("/sign-in")}
+        >
           <img src="/person-outline.svg" alt="Login" />
           <p className="ml-2">Login</p>
         </div>
       </div>
-
-      {professionals.length > 0 &&
-        professionals.map((p, index) =>
-          p.name && p.specialty && p.phone_number ? (
-            <About key={index} {...p} />
-          ) : null
-        )}
+      <div className="flex flex-col m-auto bg-white rounded shadow-md justify-around h-80vh">
+        {professionals.length > 0 &&
+          professionals.map((p, index) =>
+            p.name && p.specialty && p.phone_number ? (
+              <About key={index} {...p} />
+            ) : null
+          )}
+      </div>
     </main>
   );
 }
 
 function About(p: { specialty: string; name: string; phone_number: number }) {
   return (
-    <div className="border-2 rounded-lg flex flex-col mt-12 justify-around items-center">
-      <h3>{p.name}</h3>
-      <h4>{p.specialty}</h4>
+    <div className="ml-12">
+      <div className="border-2 rounded-lg inline-block p-2 w-auto">
+        <h3 className="text-left">{p.name}</h3>
+        <h4 className="text-left">{p.specialty}</h4>
+      </div>
     </div>
   );
 }
+
